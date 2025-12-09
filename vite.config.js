71b8d576a9271ade0826a49222ff0b69c722aed0
cheung05 <cheung1977@gmail.com>
2025-12-09 22:18:43 +0800

    config: Update Vite base path to `/weather/`.

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // 替換成您的儲存庫名稱
-  base: 'https://github.com/cheung05/weather/',
+  base: '/weather/',
   plugins: [react()],
 })
